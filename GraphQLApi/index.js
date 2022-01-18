826b7dc420cc3f4eecd21d60ee9976abfa74200c
mikaylarmcgraw <mikaylamcgraw@yahoo.com>
2022-01-18 12:12:06 -0800

    Adding detailed readme info on course

--- GraphQLApi/index.js
+++ GraphQLApi/index.js
@@ -1,6 +1,25 @@
-const {ApolloServer} = require('apollo-server');
+const {ApolloServer, gql} = require('apollo-server');
 
-const server = new ApolloServer();
+
+
+const typeDefs = gql`
+type Query{
+sessions:[Session]
+}
+type Session {
+id: ID!
+title: String!,
+description: String,
+startsAt: String,
+endsAt: String,
+room: String,
+day: String,
+format: String,
+track: String,
+level: String
+}`
+
+const server = new ApolloServer({typeDefs});
 
 server
     .listen({port: process.env.PORT || 4000})
